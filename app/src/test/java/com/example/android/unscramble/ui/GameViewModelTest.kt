8b287baf8b98e21bee2b773a5b9d2d88271c5abd
Chandan Kumar <ckm909065@gmail.com>
2023-04-12 01:52:46 +0530

    Completed SuccessPath Test Case

---
+++ app/src/test/java/com/example/android/unscramble/ui/GameViewModelTest.kt
@@ -0,0 +1,28 @@
+package com.example.android.unscramble.ui
+
+import com.example.android.unscramble.data.SCORE_INCREASE
+import com.example.android.unscramble.data.getUnscrambledWord
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Test
+
+class GameViewModelTest {
+    private val viewModel = GameViewModel()
+
+    @Test
+    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset() {
+        var currentGameUiState = viewModel.uiState.value
+        val correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+
+        viewModel.updateUserGuess(correctPlayerWord)
+        viewModel.checkUserGuess()
+        currentGameUiState = viewModel.uiState.value
+
+        assertFalse(currentGameUiState.isGuessedWordWrong)
+        assertEquals(SCORE_AFTER_FIRST_CORRECT_ANSWER, currentGameUiState.score)
+    }
+
+    companion object {
+        private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
+    }
+}
